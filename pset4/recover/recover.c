7281c544906509422da357d0f7c50f072ea7daf6
Funlamb <funlamb@gmail.com>
2021-10-11 12:10:42 -0700

    Working on learning fopen

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -5,31 +5,31 @@ typedef uint8_t BYTE;
 int main(int argc, char *argv[])
 {
     //open and closing two files practice till I know how to do it by heart
+    
     //check for 3 arguments
     if(argc != 3){
-        printf("Usage: copy SOURCE DESTINATION\n");
+        printf("usage: rec SOURCE DESTINATION");
     }
 
     //open the file
     FILE *fileSource;
     fileSource = fopen(argv[1], "r");
-    if(fileSource == NULL){
+    if(!fileSource){
         printf("Could not open source file.\n");
     }
 
     //create a destination file
     FILE *fileDestination;
     fileDestination = fopen(argv[2], "w");
-    if (fileDestination == NULL){
+    if(!fileDestination){
         fclose(fileSource);
         printf("Could not open destination file.\n");
-        return 3;
     }
 
     //copy source to destination
     BYTE buffer;
     while(fread(&buffer, sizeof(BYTE), 1, fileSource)){
-        fwrite(&buffer,sizeof(BYTE), 1, fileDestination);
+        fwrite(&buffer, sizeof(BYTE), 1, fileDestination);
     }
 
     //close files
