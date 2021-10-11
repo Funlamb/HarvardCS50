022f7054042cbae5cbc4a4973edaefd4028a7089
Funlamb <funlamb@gmail.com>
2021-10-11 13:35:51 -0700

    Still working on learning to open and close files

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -7,35 +7,37 @@ int main(int argc, char *argv[])
     //open and closing two files practice till I know how to do it by heart
     
     //check for 3 arguments
-    if(argc != 3){
+    if (argc != 3)
+    {
         printf("usage: rec SOURCE DESTINATION");
+        return 1;
     }
-
-    //open the file
+    
+    //open source file
     FILE *fileSource;
     fileSource = fopen(argv[1], "r");
-    if(!fileSource){
+    if (!fileSource)
+    {
         printf("Could not open source file.\n");
+        return 2;
     }
 
-    //create a destination file
+    //open destination file
     FILE *fileDestination;
     fileDestination = fopen(argv[2], "w");
-    if(!fileDestination){
-        fclose(fileSource);
+    if (!fileDestination)
+    {
         printf("Could not open destination file.\n");
+        fclose(fileSource);
+        return 3;
     }
 
-    //copy source to destination
     BYTE buffer;
-    while(fread(&buffer, sizeof(BYTE), 1, fileSource)){
+    while (fread(&buffer, sizeof(BYTE), 1, fileSource))
+    {
         fwrite(&buffer, sizeof(BYTE), 1, fileDestination);
     }
-
-    //close files
-    fclose(fileSource);
-    fclose(fileDestination);
-
+    
     return 0;
 
 }
