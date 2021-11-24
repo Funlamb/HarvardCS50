4ebf461a11ca6b4319862b50f865f5a653ff055b
Funlamb <funlamb@gmail.com>
2021-11-24 15:22:27 -0800

    Cleaned up the code and added comments

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -1,9 +1,7 @@
 import csv
 import sys
 
-# don't need to change str to int you can just change it when you need to check it. 
 # data.csv
-
 # name,AGATC,AATG,TATC
 # Alice,2,8,3
 # Bob,4,1,5
@@ -17,10 +15,13 @@ def main():
     if len(sys.argv) != 3:
         print("Usage: python dna.py data.csv sequence.txt")
     
+    # create a dict of suspects
     suspects = []
     with open(sys.argv[1]) as file:
         reader = csv.DictReader(file)
+        # get our SRTs to test
         SRTs = reader.fieldnames
+        # get our list of suspects
         for row in reader:
             suspects.append(row)
 
@@ -31,16 +32,19 @@ def main():
             sequence = line
 
     # get Short strands we are look to find
+    # pop the name bacuse we will never find a name in a DNA strand
     SRTs.pop(0)
-    dict_of_tests = dict.fromkeys(SRTs, 0)
+    dict_of_SRTs = dict.fromkeys(SRTs, 0)
     
     for SRT in SRTs:
         # get length of seq we are checking
         SRT_length = len(SRT)
         # find first match
-        i = 0
+        # keep track of the maximum length of SRTs in a strand
         max_SRT_count = 0
         curr_SRT_count = 0
+        # use a while loop to loop through DNA and go through indivdual letters or sets if we found a match
+        i = 0
         while i < len(sequence):
             # if matched go to next set
             if SRT == sequence[i:i+SRT_length]:
@@ -52,22 +56,23 @@ def main():
             else:
                 curr_SRT_count = 0
                 i += 1
-        dict_of_tests[SRT] = max_SRT_count
-    print(guilty_sus(suspects, dict_of_tests))
+        dict_of_SRTs[SRT] = max_SRT_count
+    print(guilty_sus(suspects, dict_of_SRTs))
 
 # Must find the longest length of STRs. Not total number of matches. 
 def guilty_sus(suspects, dict_of_srt):
+    # start name off as no match in case we do not find a match
     name = "No match"
     # compare to suspects
-    # find matches in sequence
     for sus in suspects:
+        # pop the so that the dicts only campare the SRTs
         name_of_sus = sus.pop("name")
+        # change items in the sus to ints
         for item in sus:
             sus[item] = int(sus[item])
+        #compare dicts
         if sus == dict_of_srt:
             name = name_of_sus
-    # print(suspects)
-    # print(dict_of_srt)
     return name
 
 if __name__ == "__main__":
