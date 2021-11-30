dbffaf2fd17626d4dea22f3b271a7adea8ad084a
Mike K <michaelkucukarslan@gmail.com>
2021-11-29 18:51:50 -0800

    Finished pset7 Movies

--- pset7/movies/13.sql
+++ pset7/movies/13.sql
@@ -4,8 +4,12 @@
 --     There may be multiple people named Kevin Bacon in the database. Be sure to only select the Kevin Bacon born in 1958.
 --     Kevin Bacon himself should not be included in the resulting list.
 
-SELECT name FROM people 
-SELECT title FROM movies 
-    JOIN people ON people.id = stars.person_id
-    JOIN 
-SELECT name FROM people WHERE name LIKE "kevin bacon" AND birth = 1958;
+SELECT name FROM people WHERE name != "Kevin Bacon" AND id IN ( -- Do not show Kevin Bacon but show all other names from the people's table with that id
+    SELECT DISTINCT person_id FROM stars WHERE movie_id IN ( -- Find the DISTINCT person_id's from the star's table where Kevin Bacon is also a star
+    SELECT movie_id FROM stars WHERE person_id IN( --Find movie_id's from the stars table that has the person_id of Kevin Bacon
+    SELECT id FROM people WHERE name LIKE "kevin bacon" AND birth = 1958))); --Find Kevin Bacon
+
+SELECT name FROM people WHERE name != "Kevin Bacon" AND id IN (
+    SELECT DISTINCT person_id FROM stars WHERE movie_id IN (
+    SELECT movie_id FROM stars WHERE person_id IN (
+    SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = "1958")));
