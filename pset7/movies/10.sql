ca4c38356c871a4e6b09d431736db3b8eda50cd1
Funlamb <funlamb@gmail.com>
2021-11-29 17:00:45 -0800

    Refractoring lab7 and working on movies' sqlite work

--- pset7/movies/10.sql
+++ pset7/movies/10.sql
@@ -4,6 +4,6 @@
 --     If a person directed more than one movie that received a rating of at least 9.0, they should only appear in your results once.
 SELECT DISTINCT people.name FROM people 
     JOIN directors ON directors.person_id = people.id 
-    JOIN movies ON directors.person_id = movies.id 
+    JOIN movies ON directors.movie_id = movies.id 
     JOIN ratings ON ratings.movie_id = movies.id 
     WHERE ratings.rating >= 9.0;
