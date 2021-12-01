d8e4f6c6061e70c8aac3e3c4b57f2310aa3b69ca
Mike K <michaelkucukarslan@gmail.com>
2021-11-30 19:16:38 -0800

    Pushing hard and fast

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -2,8 +2,9 @@
 
 -- All you know is that the theft took place on July 28, 2020 and that it took place on Chamberlin Street.
 
--- >.tables : To see what is in the DB
--- >.schema crime_scene_reports : To see what is in the crime_scene_reports
+tables :
+--  To see what is in the DB
+schema crime_scene_reports : To see what is in the crime_scene_reports
 
 -- CREATE TABLE crime_scene_reports (
 --     id INTEGER,
@@ -19,7 +20,7 @@
 --      Theft of the CS50 duck took place at 10:15am at the Chamberlin Street courthouse. 
 --      Interviews were conducted today with three witnesses who were present at the time — each of their interview transcripts mentions the courthouse.
 
--- >.schema interviews
+schema interviews
 -- CREATE TABLE interviews (
 --     id INTEGER,
 --     name TEXT,
@@ -30,16 +31,16 @@
 --     PRIMARY KEY(id)
 -- );
 
--- >SELECT year, transcript FROM interviews;
+SELECT year, transcript FROM interviews;
 -- Just wanted to see how many interviews there were in the DB
 
 -- >.schema
 -- Wanted to see how the DB was orginized
 
--- SELECT name FROM interviews WHERE day = 28 AND year = 2020 AND month = 7;
+SELECT name FROM interviews WHERE day = 28 AND year = 2020 AND month = 7;
 -- Forgot to add the transcript.
 
--- SELECT name, transcript FROM interviews WHERE day = 28 AND year = 2020 AND month = 7;
+SELECT name, transcript FROM interviews WHERE day = 28 AND year = 2020 AND month = 7;
 
 -- Ruth|Sometime within ten minutes of the theft, I saw the thief get into a car in the courthouse parking lot and drive away. 
 --      If you have security footage from the courthouse parking lot, you might want to look for cars that left the parking lot in that time frame.
@@ -49,7 +50,7 @@
 --      In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. 
 --      The thief then asked the person on the other end of the phone to purchase the flight ticket.
 
--- SELECT caller, receiver, duration FROM phone_calls WHERE day = 28 AND month = 7 AND year = 2020;
+SELECT caller, receiver, duration FROM phone_calls WHERE day = 28 AND month = 7 AND year = 2020;
 -- Too complicated to make an assumption on who it is. Need to figure out what the duration integer is in units.
 
 SELECT license_plate, activity FROM courthouse_security_logs WHERE year = 2020 AND month = 7 AND day = 28 AND hour = 10;
@@ -62,5 +63,14 @@ SELECT atm_location FROM atm_transactions;
 -- Looked at what locations were in DB
 
 SELECT transaction_type, amount FROM atm_transactions WHERE atm_location LIKE "Fifer Street" AND year = 2020 AND month = 7 AND day = 28;
--- Showed transactions but nothing of inrest yet
+-- Showed transactions but nothing of intrest yet
+
+SELECT city FROM airports;
+-- Want to see the airports
+
+SELECT flights.origin_airport_id, flights.destination_airport_id, flights.hour, flights.minute FROM flights 
+    JOIN airports ON flights.origin_airport_id = airports.id
+    WHERE year = 2020 AND month = 7 AND day = 29
+    ORDER BY flights.hour ASC;
+-- Find out what flights were leaving the next day
 
