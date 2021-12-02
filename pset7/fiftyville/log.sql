6bac661e473752bbe840abff839d2b93196388b4
Funlamb <funlamb@gmail.com>
2021-12-01 16:43:07 -0800

    Sucsessfully completed pset7 Fiftyville one the first try

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -16,7 +16,7 @@ schema crime_scene_reports : To see what is in the crime_scene_reports
 --     PRIMARY KEY(id)
 -- );
 
--- >SELECT description FROM crime_scene_reports WHERE YEAR = 2020 AND month = 7 AND day = 28 AND street LIKE "Chamberlin Street";
+SELECT description FROM crime_scene_reports WHERE YEAR = 2020 AND month = 7 AND day = 28 AND street LIKE "Chamberlin Street";
 --      Theft of the CS50 duck took place at 10:15am at the Chamberlin Street courthouse. 
 --      Interviews were conducted today with three witnesses who were present at the time — each of their interview transcripts mentions the courthouse.
 
@@ -74,3 +74,54 @@ SELECT flights.origin_airport_id, flights.destination_airport_id, flights.hour, 
     ORDER BY flights.hour ASC;
 -- Find out what flights were leaving the next day
 
+-- Left parking lot within ten minutes, withdrew money from bank, called a friend for less than 1 minute
+-- Join courthouse_security_logs with people by licence_plate, Join people to phone_calls
+SELECT people.name, phone_calls.receiver,  FROM people 
+    JOIN courthouse_security_logs ON people.license_plate = courthouse_security_logs.license_plate
+    JOIN phone_calls ON phone_calls.caller = people.phone_number
+    JOIN bank_accounts ON bank_accounts.person_id = people.id 
+    JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
+    WHERE phone_calls.year = 2020 AND phone_calls.month = 7 AND phone_calls.day = 28 -- Shows right day
+    AND phone_calls.duration <= 60 -- Shows right phone call duration
+    
+-- Join courthouse_security_logs with people by licence_plate, Join people to phone_calls, Join bank_accounts with people, 
+SELECT people.name, phone_calls.receiver,  FROM people 
+    JOIN courthouse_security_logs ON people.license_plate = courthouse_security_logs.license_plate
+    JOIN phone_calls ON phone_calls.caller = people.phone_number
+    JOIN bank_accounts ON bank_accounts.person_id = people.id 
+    JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
+    WHERE phone_calls.year = 2020 AND phone_calls.month = 7 AND phone_calls.day = 28 -- Shows right day
+    AND phone_calls.duration <= 60 -- Shows right phone call duration
+    AND courthouse_security_logs.activity LIKE "exit" -- Shows correct direction of the car
+    AND courthouse_security_logs.year = 2020 AND courthouse_security_logs.month = 7 AND courthouse_security_logs.day = 28 -- Shows right day at courthouse
+    AND courthouse_security_logs.hour = 10 AND courthouse_security_logs.minute <= 25 -- Shows the right time at courthouse
+    AND atm_transactions.transaction_type LIKE "withdraw";
+
+-- Join courthouse_security_logs with people by licence_plate, Join people to phone_calls, Join bank_accounts with people,  Join passangers to flights,
+SELECT people.name, phone_calls.receiver, airports.city, flights.day FROM people 
+    JOIN passengers ON passengers.passport_number = people.passport_number
+    JOIN courthouse_security_logs ON people.license_plate = courthouse_security_logs.license_plate
+    JOIN phone_calls ON phone_calls.caller = people.phone_number
+    JOIN bank_accounts ON bank_accounts.person_id = people.id 
+    JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
+    JOIN flights ON passengers.flight_id = flights.id
+    JOIN airports ON flights.destination_airport_id = airports.id
+    WHERE phone_calls.year = 2020 AND phone_calls.month = 7 AND phone_calls.day = 28 -- Shows right day
+    AND phone_calls.duration <= 60 -- Shows right phone call duration
+    AND courthouse_security_logs.activity LIKE "exit" -- Shows correct direction of the car
+    AND courthouse_security_logs.year = 2020 AND courthouse_security_logs.month = 7 AND courthouse_security_logs.day = 28 -- Shows right day at courthouse
+    AND courthouse_security_logs.hour = 10 AND courthouse_security_logs.minute <= 25 -- Shows the right time at courthouse
+    AND atm_transactions.transaction_type LIKE "withdraw";
+-- Getting errors in my code. I think I need to try and find each piece individually 
+-- Fixed the code but the view is too narrow. I think I need to find each piece individually
+-- Ernest|(375) 555-8161|London|29
+-- Ernest|(375) 555-8161|London|29
+-- Russell|(725) 555-3243|Boston|29
+-- Russell|(725) 555-3243|Fiftyville|30
+-- Russell|(725) 555-3243|Dallas|30
+-- I'm pretty sure its Ernest going to London calling whoever is at the 375 number
+
+SELECT people.name FROM people
+    WHERE people.phone_number LIKE "(375) 555-8161";
+-- Berthold
+    
