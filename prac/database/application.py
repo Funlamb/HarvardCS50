66d3f72a92b5fff6af0d2dc92a9ddbc8a3741c4e
Funlamb <funlamb@gmail.com>
2021-12-20 14:00:38 -0800

    Butified the edit page. Cleaned up a placeholder bug

--- prac/database/application.py
+++ prac/database/application.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 # ensure templates are reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 # Configure DB
 db = SQL("sqlite:///birthdays.db")
@@ -46,13 +47,27 @@ def delete():
 def edit_screen():
     if request.method == "POST":
         id = request.form.get("editID")
-        birthday = db.execute("SELECT * FROM birthdays WHERE id=?", id)
-        return render_template("edit.html", birthday=birthday)
+        # birthday = db.execute("SELECT * FROM birthdays WHERE id=?", id)
+        session['my_var'] = id
+        return redirect('/edit')
 
 @app.route("/edit", methods=["GET", "POST"])
 def edit():
+    if request.method == "GET":
+        id = session.get('my_var', None)
+        birthday = db.execute("SELECT * FROM birthdays WHERE id=?", id)
+        for item in birthday:
+            print (item)
+
+        return render_template("/edit.html", birthday=birthday)
+
+@app.route("/edit_DB", methods=["GET", "POST"])
+def edit_DB():
     if request.method == "POST":
-        id = request.form.get("editID")
-
-
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+        edit_id = request.form.get("edit_id")
+        print (name + month + day + edit_id)
+        db.execute("UPDATE birthdays SET name = ?, month = ?, day = ? WHERE id=?", name, month, day, edit_id)
         return redirect("/")
